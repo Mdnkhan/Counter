11879a0e70428f660a315fd1c579fdb717401a0d
unknown <sakib.feb97@gmail.com>
2020-02-20 20:14:51 -0800

    added the intreament and decreament methods!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, {Component} from 'react'
 
 class Counter extends Component{
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increament = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  decreament = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+  clearCount = () => {
+    this.setState({
+      count: 0
+    })
+  }
   render(){
     return(
-      <div></div>
-    )
+      <div className = "container">
+        <div className = "navbar">Counter.js</div>
+        <div className = "counter">
+          <h1>{this.state.count}</h1>
+          <button type = "button" onClick={this.increament}>Increament</button>
+          <button type = "button" onClick={this.decreament}>Decreament</button>
+          <button type = "button" onClick={this.clearCount}>Clear</button>
+        </div>
+
+      </div>
+    );
   }
-}
+};
 
 export default Counter;
